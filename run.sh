05592d1585bf17467f6ee19dce35d28accf22de9
Alexandre Pereira LLemes <alexandre-pl@segplan.go.gov.br>
2017-12-18 18:36:52 -0200

    Correcoes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compila a imagem
-docker build -t sei-docker-debian --build-arg HOME=$HOME --build-arg USERPROXY=USUARIO.REDE --build-arg PASSWD=SENHA.REDE --build-arg PROJETO=sei .
+docker build -t projeto-docker-debian --build-arg HOME=$HOME --build-arg USERPROXY=USUARIO.REDE --build-arg PASSWD=SENHA.REDE --build-arg PROJETO=projeto .
 
 # Fecha o container
 docker stop apache
@@ -10,10 +10,10 @@ docker stop apache
 docker rm apache
 
 # Cria o container
-docker run -it --name apache -p 80:80 --network=host --privileged -v $HOME/discoDocker/apache/projetos/:/var/www/html/ -v $HOME/discoDocker/apache/conf/:/etc/apache2/sites-available -v /tmp:/tmp -d sei-docker-debian:latest
+docker run -it --name apache -p 80:80 --network=host --privileged -v $HOME/discoDocker/apache/projetos/:/var/www/html/ -v $HOME/discoDocker/apache/conf/:/etc/apache2/sites-available -v /tmp:/tmp -d projeto-docker-debian:latest
 
-# Habilita o ambiente seides.intra.goias.gov.br
-docker exec -it apache a2ensite seides.intra.goias.gov.br
+# Habilita o ambiente projeto.teste.com.br
+docker exec -it apache a2ensite projeto.teste.com.br
 
 # Reinicia os servicos
 docker exec -it apache /etc/init.d/apache2 restart
