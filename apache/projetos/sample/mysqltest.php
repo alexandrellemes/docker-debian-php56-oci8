0b2abfa74850e95424ece0300222a18b15ac33c7
alexandrellemes <g1I@t3123@>
2020-11-27 11:43:31 -0300

    # Exemplo de uso com o MYSQL e a configuração de CHARSET.

--- apache/projetos/sample/mysqltest.php
+++ apache/projetos/sample/mysqltest.php
@@ -1,12 +1,23 @@
 <?php
 
+$mysqli = new mysqli("mysqldb", "root", "123456", "mysql");
+
+/* change character set to utf8 */
+if (!$mysqli->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $mysqli->error);
+    exit();
+} else {
+    printf("Current character set: %s\n", $mysqli->character_set_name());
+}
+
 // Cria uma conexão com o servidor.
 $conn = mysqli_connect('mysqldb', 'root', '123456');
+mysqli_set_charset($conn, "utf8");
 
 // Verifica a conexão
 if ($conn == false) {
     die("Não foi possível conectar ao banco: " . mysqli_connect_error());
 } else {
     mysqli_close($conn);
-    echo 'Acesso ao banco MYSQL - OK.';
+    echo PHP_EOL . '<br>Acesso ao banco MYSQL - OK.';
 }
